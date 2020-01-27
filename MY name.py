6f77b15b329a665f71149ec2d88cca6b84b530f3
Sergey <53119472+Serhio02@users.noreply.github.com>
2020-01-27 14:07:56 +0300

    Add files via upload

---
+++ MY name.py
@@ -0,0 +1,11 @@
+#This program says hello and asks for my name.
+
+print("Hellow World")
+print("What is your name?")   #ask for their name.
+myName = input()
+print("It is good to meet you, " + myName)
+print("the leng of your name is:")
+print(len(myName))
+print("what is your age?")
+myAge = input()
+print("you will be " + str(int(myAge) + 1) + "in a year.")
---
+++ ifSpam.py
@@ -0,0 +1,7 @@
+spam = int(input())
+if spam == 1:
+    print('Hello')
+if spam == 2:
+    print('Howdy')
+else:
+    print('Greetings!')
